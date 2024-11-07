dd292a6f8a8e5aa800471cc2b128b397dbdccf78
EK <58846492+rd800919@users.noreply.github.com>
2024-11-07 12:05:43 +0800

    Update run_latest_iptables_setup.sh

--- run_latest_iptables_setup.sh
+++ run_latest_iptables_setup.sh
@@ -6,16 +6,23 @@ URL="https://raw.githubusercontent.com/rd800919/script/refs/heads/main/iptables-
 # 设置保存的文件名
 FILE="iptables-setup2.sh"
 
+# 确认并删除旧文件
+if [ -f "$FILE" ]; then
+    echo "旧文件存在，正在删除..."
+    rm -f "$FILE"
+fi
+
 # 下载文件
 echo "正在下载最新的脚本..."
-wget -O $FILE $URL
+if wget -O "$FILE" "$URL"; then
+    echo "下载完成。"
 
-# 赋予脚本执行权限
-chmod +x $FILE
+    # 赋予脚本执行权限
+    chmod +x "$FILE"
 
-# 提示下载完成
-echo "下载完成。"
-
-# 运行脚本
-echo "运行脚本..."
-./$FILE
+    # 运行脚本
+    echo "运行脚本..."
+    ./"$FILE"
+else
+    echo "下载失败，请检查网络连接或URL是否正确。"
+fi
